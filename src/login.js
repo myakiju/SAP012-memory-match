6bc2fd4f09dd219ea13eba00ef5df037f5dab218
Thaysa Borges <borges.thaysa@gmail.com>
2024-03-21 12:48:56 -0300

    Modifica a página de login para ser a página principal

--- src/login.js
+++ src/login.js
@@ -14,7 +14,7 @@ const validateInput = ({ target }) => {
 const handleSubmit = (event) => {
    event.preventDefault(); // evento que serve para bloquear o evento padrão do formulário
    localStorage.setItem('player' , input.value); //o método setItem serve para salvar um novo item no localstorage. Ele espera dois parametros: o primeiro é a chave o valor que quero salvar (nesse caso é o player) que é o nome que a pessoa digitar no input
-   window.location = 'index.html';
+   window.location = 'game.html';
 }
 
 input.addEventListener('input' , validateInput);
